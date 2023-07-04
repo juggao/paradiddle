2df31d2568a0ddfa6bf9c31d0b9149c6a30bf6ae
RO <49290325+juggao@users.noreply.github.com>
2023-07-04 18:41:17 +0200

    Add files via upload

--- lr2randmidi.py
+++ lr2randmidi.py
@@ -1,5 +1,5 @@
 from midiutil import MIDIFile
-import random, sys
+import random, sys, argparse
 
 #Key# Note Drum Sound Key# Note Drum Sound
 #35 B0 Acoustic Bass Drum 59 B2 Ride Cymbal 2
@@ -28,6 +28,20 @@ import random, sys
 #58 Bb2 Vibraslap
 
 bpm = 155
+filename = ""
+verbose = 0
+output_file = "about.mid"
+
+parser = argparse.ArgumentParser(
+                    prog='l2randmidi.py',
+                    description='Generates a MIDI file from a "LLRRLLLRR" string',
+                    epilog='(c) 2023 René Oudeweg')
+
+parser.add_argument('filename')           # positional argument
+parser.add_argument('-b', '--bpm')      # option that takes a value
+parser.add_argument('-v', '--verbose')
+parser.add_argument('-o', '--output')
+
 
 def pairwise(iterable):
     "s -> (s0, s1), (s2, s3), (s4, s5), ..."
@@ -72,7 +86,8 @@ def translate_to_midi(text, output_file):
                     midi.addNote(track, channel, note, time, duration, random_volume)
                     time += duration
                 text = text[2:]    
-        print("Writing MIDI notes: \n"+notestring)
+        if verbose:
+            print("Writing MIDI notes to :"+output_file+"\n"+notestring)
     except ValueError:
         pass
         
@@ -82,14 +97,22 @@ def translate_to_midi(text, output_file):
         midi.writeFile(file)
 
 
-filename= ""
 # Check if filename is provided as a command-line argument
-if len(sys.argv) >= 2:
-    filename = sys.argv[1]          
-if len(sys.argv) >= 3:
-    bpm = int(sys.argv[2])    
+args = parser.parse_args()
+print(args.filename, args.bpm, args.verbose, args.output)
+if args.bpm != 0:
+    bpm = int(args.bpm)
+if not args.filename:
+    filename = ""
+else:
+    filename = args.filename
+if args.verbose:
+    verbose = 1
+if args.output:
+    output_file = args.output
 try:
-    print("opening: "+filename)
+    if verbose:
+        print("opening: "+filename)
     with open(filename, 'r') as file:
         lrstring = file.read()
 except FileNotFoundError:
@@ -98,7 +121,8 @@ except FileNotFoundError:
 except IOError:
     print("Error reading the file.")
             
-output_file = 'about.mid'
-print(lrstring)
+
+if verbose:
+    print(lrstring)
 translate_to_midi(lrstring, output_file)
 
